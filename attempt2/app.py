7d28ab90076de06a217ab3da18e2833d4f4bb8a3
ericaokeh <157079075+ericaokeh@users.noreply.github.com>
2025-03-22 19:57:59 -0400

    End

--- attempt2/app.py
+++ attempt2/app.py
@@ -9,6 +9,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
+
 # Initialize the app
 st.set_page_config(
     page_title="CommuniTrade - Buy & Barter Marketplace",
